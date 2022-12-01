164d6a66c93335f9adbb2ee3167ae42030f0df71
YetiBarBar <yetibarbar@gmail.com>
2022-12-01 19:54:59 +0100

    Add structure and first day

---
+++ src/day1.rs
@@ -0,0 +1,17 @@
+pub fn main() {
+    let calories: std::collections::BinaryHeap<u64> = include_str!("../data/day_2022_1.data")
+        .split("\n\n")
+        .map(|block| {
+            block
+                .lines()
+                .map(str::parse::<u64>)
+                .map(Result::unwrap)
+                .sum::<u64>()
+        })
+        .collect();
+
+    let calories = calories.into_sorted_vec();
+
+    println!("Part 1: {}", calories.iter().rev().take(1).sum::<u64>());
+    println!("Part 2: {}", calories.iter().rev().take(3).sum::<u64>());
+}
