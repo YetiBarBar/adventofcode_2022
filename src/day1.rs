01cadac51ca0c709560f30367bc6bcdd583c5284
YetiBarBar <yetibarbar@gmail.com>
2022-12-02 21:12:20 +0100

    Refactor day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,13 @@
 pub fn main() {
-    let calories: std::collections::BinaryHeap<u64> = include_str!("../data/day_2022_1.data")
+    let calories = parse_data();
+    let calories = calories.into_sorted_vec();
+
+    println!("Part 1: {}", calories.iter().rev().take(1).sum::<u64>());
+    println!("Part 2: {}", calories.iter().rev().take(3).sum::<u64>());
+}
+
+fn parse_data() -> std::collections::BinaryHeap<u64> {
+    let calories = include_str!("../data/day_2022_1.data")
         .split("\n\n")
         .map(|block| {
             block
@@ -9,9 +17,5 @@ pub fn main() {
                 .sum::<u64>()
         })
         .collect();
-
-    let calories = calories.into_sorted_vec();
-
-    println!("Part 1: {}", calories.iter().rev().take(1).sum::<u64>());
-    println!("Part 2: {}", calories.iter().rev().take(3).sum::<u64>());
+    calories
 }
