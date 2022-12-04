00188aeb6b4a8937eb6ef86787fd58dd0dc586d7
YetiBarBar <yetibarbar@gmail.com>
2022-12-04 16:05:03 +0100

    Add solution for day 4

--- src/day4.rs
+++ src/day4.rs
@@ -1,3 +1,71 @@
+use adventofcode_tooling::AocError;
+use std::str::FromStr;
+
+struct Segment {
+    begin: u32,
+    end: u32,
+}
+
+impl FromStr for Segment {
+    type Err = AocError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let Some((begin, end)) = s.split_once('-') else {
+            return Err(AocError::ParsingError);
+        };
+
+        Ok(Self {
+            begin: begin.parse()?,
+            end: end.parse()?,
+        })
+    }
+}
+
+impl Segment {
+    fn is_included(&self, other: &Segment) -> bool {
+        self.begin >= other.begin && self.end <= other.end
+    }
+
+    fn is_overlapping(&self, other: &Segment) -> bool {
+        self.begin <= other.end && self.end >= other.begin
+    }
+}
+
+struct SegmentPair(Segment, Segment);
+
+impl SegmentPair {
+    fn has_inclusion(&self) -> bool {
+        self.1.is_included(&self.0) || self.0.is_included(&self.1)
+    }
+
+    fn has_overlap(&self) -> bool {
+        self.1.is_overlapping(&self.0) || self.0.is_overlapping(&self.1)
+    }
+}
+
+impl FromStr for SegmentPair {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (one, two) = s.split_once(',').unwrap();
+        Ok(Self(one.parse().unwrap(), two.parse().unwrap()))
+    }
+}
+
 pub fn main() {
-    todo!()
+    let data: Vec<SegmentPair> = include_str!("../data/day_2022_4.data")
+        .lines()
+        .map(str::parse)
+        .map(Result::unwrap)
+        .collect();
+
+    println!(
+        "Part 1: {}",
+        data.iter().filter(|pair| pair.has_inclusion()).count()
+    );
+
+    println!(
+        "Part 2: {}",
+        data.iter().filter(|pair| pair.has_overlap()).count()
+    );
 }
