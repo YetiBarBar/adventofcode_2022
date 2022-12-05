5330a99ef9bcd920989b8545f936c27f427143cc
YetiBarBar <yetibarbar@gmail.com>
2022-12-05 19:54:51 +0100

    Add solution for day 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,19 +1,18 @@
 use nom::{
     bytes::complete::tag,
-    character::complete::{char, u64},
+    character::complete::{char, u16},
     combinator::map,
     multi::separated_list0,
     sequence::tuple,
     IResult,
 };
-use std::collections::HashMap;
 
-fn moves(data: &str) -> IResult<&str, Vec<(u64, u64, u64)>> {
+fn parse_moves(data: &str) -> IResult<&str, Vec<(usize, usize, usize)>> {
     separated_list0(
         char('\n'),
         map(
-            tuple((tag("move "), u64, tag(" from "), u64, tag(" to "), u64)),
-            |(_, times, _, start, _, end)| (times, start, end),
+            tuple((tag("move "), u16, tag(" from "), u16, tag(" to "), u16)),
+            |(_, times, _, start, _, end)| (times.into(), start.into(), end.into()),
         ),
     )(data)
 }
@@ -24,44 +23,37 @@ pub fn main() {
         .unwrap()
         .1;
 
-    let moves = moves(data).unwrap().1;
+    let moves = parse_moves(data).unwrap().1;
 
-    let stacks = (1_u64..)
-        .zip([
-            "DZTH", "QRWYGCS", "PBFQNRCH", "LCNFHZ", "GLFQS", "VPWZBRCS", "ZFJ", "DLVZRHQ",
-            "BHGNFZLD",
-        ])
-        .map(|(idx, chrs)| (idx, chrs.chars().collect::<Vec<char>>()))
-        .collect::<HashMap<u64, Vec<char>>>();
+    let stacks = [
+        "DZTH", "QRWYGCS", "PBFQNRCH", "LCNFHZ", "GLFQS", "VPWZBRCS", "ZFJ", "DLVZRHQ", "BHGNFZLD",
+    ]
+    .iter()
+    .map(|chrs| chrs.chars().collect::<Vec<char>>())
+    .collect::<Vec<Vec<char>>>();
 
     let mut part1_stacks = stacks.clone();
     for instr in &moves {
-        for _ in 0..instr.0 {
-            let value = part1_stacks.get_mut(&instr.1).unwrap().pop().unwrap();
-            part1_stacks.get_mut(&instr.2).unwrap().push(value);
-        }
-    }
-
-    println!("Part 1: {}", hmap_to_part_result(&part1_stacks));
-
-    let mut part2_stacks = stacks.clone();
-    for instr in &moves {
-        let mut vals = vec![];
-        for _ in 0..instr.0 {
-            let value = part2_stacks.get_mut(&instr.1).unwrap().pop().unwrap();
-            vals.push(value);
-        }
-        part2_stacks
-            .get_mut(&instr.2)
+        let curent_stack = part1_stacks.get_mut(instr.1 - 1).unwrap();
+        let vals = curent_stack.split_off(curent_stack.len() - instr.0);
+        part1_stacks
+            .get_mut(instr.2 - 1)
             .unwrap()
             .extend(vals.iter().rev());
     }
 
-    println!("Part 2: {}", hmap_to_part_result(&part2_stacks));
+    println!("Part 1: {}", to_part_result(&part1_stacks));
+
+    let mut part2_stacks = stacks;
+    for instr in &moves {
+        let curent_stack = part2_stacks.get_mut(instr.1 - 1).unwrap();
+        let vals = curent_stack.split_off(curent_stack.len() - instr.0);
+        part2_stacks.get_mut(instr.2 - 1).unwrap().extend(vals);
+    }
+
+    println!("Part 2: {}", to_part_result(&part2_stacks));
 }
 
-fn hmap_to_part_result(hmap: &HashMap<u64, Vec<char>>) -> String {
-    (1..=9)
-        .map(|idx| hmap.get(&idx).unwrap().last().unwrap())
-        .collect()
+fn to_part_result(stacks: &[Vec<char>]) -> String {
+    stacks.iter().map(|stack| stack.last().unwrap()).collect()
 }
