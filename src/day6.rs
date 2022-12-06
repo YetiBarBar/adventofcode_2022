7c28975190b73b9e554fea0282eba3d076f47192
YetiBarBar <yetibarbar@gmail.com>
2022-12-06 07:23:03 +0100

    Add solution for day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,3 +1,18 @@
+use hashbrown::HashSet;
+
 pub fn main() {
-    todo!()
+    // let raw = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    let raw = include_str!("../data/day_2022_6.data");
+    let raw: Vec<char> = raw.chars().collect();
+    println!("Part 1: {}", answer(&raw, 4));
+    println!("Part 2: {}", answer(&raw, 14));
+}
+
+fn answer(data: &[char], len: usize) -> usize {
+    data.windows(len)
+        .enumerate()
+        .find(|(_, win)| win.iter().collect::<HashSet<_>>().len() == len)
+        .unwrap()
+        .0
+        + len
 }
