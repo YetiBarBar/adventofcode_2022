660cce470e8fe6348e42b5a22babb0dead8be2a6
YetiBarBar <yetibarbar@gmail.com>
2022-12-10 21:32:39 +0100

    Add solution for day 9

--- src/day8.rs
+++ src/day8.rs
@@ -1,3 +1,68 @@
+use adventofcode_tooling::Matrix2D;
 pub fn main() {
-    todo!()
+    /*     let matrix = Matrix2D {
+            values: r#"30373
+    25512
+    65332
+    33549
+    35390"#
+                .lines()
+                .flat_map(|line| line.chars().map(|v| v.to_digit(10).unwrap()))
+                .collect(),
+            height: 5,
+            width: 5,
+        }; */
+    let matrix = Matrix2D {
+        values: include_str!("../data/day_2022_8.data")
+            .lines()
+            .flat_map(|line| line.chars().map(|v| v.to_digit(10).unwrap()))
+            .collect(),
+        height: 99,
+        width: 99,
+    };
+    let visible_count = part_1(&matrix);
+
+    println!("Part 1: {}", visible_count);
+
+    let part2 = (0..matrix.values.len())
+        .map(|position| (position % matrix.width, position / matrix.width))
+        .map(|(x, y)| tree_scenic(&matrix, x, y))
+        .max();
+    println!("Part 2: {:?}", part2);
+}
+
+fn part_1(matrix: &Matrix2D<u32>) -> i32 {
+    let mut visible_count = 0;
+    for position in 0..matrix.values.len() {
+        let (x, y) = (position % matrix.width, position / matrix.width);
+
+        let row = matrix.row(y);
+        let col = matrix.col(x);
+        let current = row[x];
+
+        if row[..x].iter().all(|v| current > *v)
+            || row[x + 1..].iter().all(|v| current > *v)
+            || col[..y].iter().all(|v| current > *v)
+            || col[y + 1..].iter().all(|v| current > *v)
+        {
+            visible_count += 1;
+        }
+    }
+    visible_count
+}
+
+fn tree_scenic(matrix: &Matrix2D<u32>, x: usize, y: usize) -> usize {
+    let row = matrix.row(y);
+    let col = matrix.col(x);
+    let current = row[x];
+    let x1 = row[x + 1..].iter().take_while(|v| **v < current).count()
+        + usize::from(row[x + 1..].iter().any(|v| *v >= current));
+    let x2 = row[..x].iter().rev().take_while(|v| **v < current).count()
+        + usize::from(row[..x].iter().rev().any(|v| *v >= current));
+    let y1 = col[y + 1..].iter().take_while(|v| **v < current).count()
+        + usize::from(col[y + 1..].iter().any(|v| *v >= current));
+    let y2 = col[..y].iter().rev().take_while(|v| **v < current).count()
+        + usize::from(col[..y].iter().rev().any(|v| *v >= current));
+
+    x1 * x2 * y1 * y2
 }
--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,99 @@
+use std::str::FromStr;
+
+use hashbrown::HashSet;
+
+struct Rope {
+    knots: Vec<(isize, isize)>,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Movement {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+impl FromStr for Movement {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let m = s.split_ascii_whitespace().next().unwrap();
+        match m {
+            "R" => Ok(Self::Right),
+            "U" => Ok(Self::Up),
+            "D" => Ok(Self::Down),
+            "L" => Ok(Self::Left),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Rope {
+    fn new() -> Self {
+        Self {
+            knots: vec![(0, 0); 11],
+        }
+    }
+    fn make_move(&mut self, movement: Movement) {
+        match movement {
+            Movement::Up => self.knots[0].1 += 1,
+            Movement::Down => self.knots[0].1 -= 1,
+            Movement::Right => self.knots[0].0 += 1,
+            Movement::Left => self.knots[0].0 -= 1,
+        }
+
+        let mut previous = self.knots[0];
+        for knot in self.knots.iter_mut().skip(1) {
+            if (previous.1 - knot.1).abs() > 1 || (previous.0 - knot.0).abs() > 1 {
+                match (previous.0.cmp(&knot.0), previous.1.cmp(&knot.1)) {
+                    (std::cmp::Ordering::Equal, std::cmp::Ordering::Equal) => (),
+                    (std::cmp::Ordering::Equal, std::cmp::Ordering::Greater) => knot.1 += 1,
+                    (std::cmp::Ordering::Equal, std::cmp::Ordering::Less) => knot.1 -= 1,
+                    (std::cmp::Ordering::Less, std::cmp::Ordering::Equal) => knot.0 -= 1,
+                    (std::cmp::Ordering::Greater, std::cmp::Ordering::Equal) => knot.0 += 1,
+                    (std::cmp::Ordering::Less, std::cmp::Ordering::Less) => {
+                        knot.1 -= 1;
+                        knot.0 -= 1;
+                    }
+                    (std::cmp::Ordering::Greater, std::cmp::Ordering::Greater) => {
+                        knot.1 += 1;
+                        knot.0 += 1;
+                    }
+                    (std::cmp::Ordering::Less, std::cmp::Ordering::Greater) => {
+                        knot.1 += 1;
+                        knot.0 -= 1;
+                    }
+
+                    (std::cmp::Ordering::Greater, std::cmp::Ordering::Less) => {
+                        knot.1 -= 1;
+                        knot.0 += 1;
+                    }
+                }
+            }
+            previous = *knot;
+        }
+    }
+}
+
 pub fn main() {
-    todo!()
+    let moves: Vec<(Movement, usize)> = include_str!("../data/day_2022_9.data")
+        .lines()
+        .map(|l| l.split_once(' ').unwrap())
+        .map(|(left, right)| (left.parse().unwrap(), right.parse().unwrap()))
+        .collect();
+    let mut knot = Rope::new();
+    let mut part1 = HashSet::new();
+    let mut part2 = HashSet::new();
+
+    for (mvt, times) in moves {
+        for _ in 0..times {
+            knot.make_move(mvt);
+            part1.insert(knot.knots[1]);
+            part2.insert(knot.knots[9]);
+        }
+    }
+
+    println!("Part 1: {}", part1.len());
+    println!("Part 2: {}", part2.len());
 }
