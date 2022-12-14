edcdb44da42cbbd9c6360abd37bedbf462f7206e
YetiBarBar <yetibarbar@gmail.com>
2022-12-14 17:43:13 +0100

    Refactor day 14

--- src/day14.rs
+++ src/day14.rs
@@ -8,6 +8,11 @@ use nom::{
     IResult,
 };
 
+enum Part {
+    Part1,
+    Part2,
+}
+
 #[derive(Debug)]
 struct Segment {
     points: Vec<(u32, u32)>,
@@ -31,7 +36,7 @@ fn fill_matrix(matrix: &mut Matrix2D<char>, segments: &[Segment]) {
                 let ymin = p2.1.min(p1.1) as usize;
                 let ymax = p2.1.max(p1.1) as usize;
                 for y in ymin..=ymax {
-                    matrix.values[y * matrix.width + x] = '#';
+                    matrix.values[y * matrix.width + x] = '█';
                 }
             } else if p1.1 == p2.1 {
                 let y = p1.1 as usize;
@@ -39,7 +44,7 @@ fn fill_matrix(matrix: &mut Matrix2D<char>, segments: &[Segment]) {
                 let xmin = p2.0.min(p1.0) as usize;
                 let xmax = p2.0.max(p1.0) as usize;
                 for x in xmin..=xmax {
-                    matrix.values[y * matrix.width + x] = '#';
+                    matrix.values[y * matrix.width + x] = '█';
                 }
             } else {
                 panic!("Unsupported non linear!")
@@ -48,7 +53,8 @@ fn fill_matrix(matrix: &mut Matrix2D<char>, segments: &[Segment]) {
     }
 }
 
-fn fall_point(matrix: &mut Matrix2D<char>, part2: bool) -> bool {
+fn fall_point(matrix: &mut Matrix2D<char>, part: &Part) -> bool {
+    let part2 = matches!(part, Part::Part2);
     let (mut coord_x, mut coord_y) = (500, 0);
     loop {
         let (left, center, right) = (
@@ -56,12 +62,12 @@ fn fall_point(matrix: &mut Matrix2D<char>, part2: bool) -> bool {
             matrix.get(coord_x, coord_y + 1).unwrap(),
             matrix.get(coord_x + 1, coord_y + 1).unwrap(),
         );
-        if matrix.get(500, 0) == Some('o') {
+        if matrix.get(500, 0) == Some('░') {
             return false;
         }
         if left != ' ' && center != ' ' && right != ' ' || (coord_y == matrix.height - 2 && part2) {
             // Then we have a support and put a ball!
-            matrix.values[coord_y * matrix.width + coord_x] = 'o';
+            matrix.values[coord_y * matrix.width + coord_x] = '░';
             return true;
         } else if left == ' ' && center != ' ' {
             // we have room to fall left
@@ -77,70 +83,40 @@ fn fall_point(matrix: &mut Matrix2D<char>, part2: bool) -> bool {
     false
 }
 
-pub fn main() {
-    let data = include_str!("../data/day_2022_14.data");
-    let segments = all_consuming(separated_list0(newline, segment))(data)
-        .unwrap()
-        .1;
-
-    //
-    // Look for xmin, xmax, ymin, ymin
-    let (_, _, _, ymax) = segments.iter().flat_map(|s| s.points.iter()).fold(
-        (u32::MAX, 0, u32::MAX, 0),
-        |(xmin, xmax, ymin, ymax), (x, y)| (xmin.min(*x), xmax.max(*x), ymin.min(*y), ymax.max(*y)),
-    );
-
-    //
-    // Now we know xmin: 447, xmax: 515, ymin: 13, ymax = 168
+fn run_part(segments: &[Segment], ymax: usize, part: Part) -> usize {
     // Let's fill a matrix with walls!
     let mut matrix = Matrix2D {
         width: 1000,
         height: ymax as usize + 3,
         values: vec![' '; 1000 * (ymax as usize + 3)],
     };
-
     fill_matrix(&mut matrix, &segments);
 
     let mut count = 0;
-    for _ in 0..10_000 {
+    while fall_point(&mut matrix, &part) {
         count += 1;
-        if !fall_point(&mut matrix, false) {
-            break;
-        }
     }
+    count
+}
 
-    // Last ball should not be counted!
-    println!("Part 1: {}", count - 1);
-
-    /*****************************************
-     * Part 2
-     *****************************************/
+pub fn main() {
+    let data = include_str!("../data/day_2022_14.data");
     let segments = all_consuming(separated_list0(newline, segment))(data)
         .unwrap()
         .1;
 
-    //
     // Look for xmin, xmax, ymin, ymin
     let (_, _, _, ymax) = segments.iter().flat_map(|s| s.points.iter()).fold(
         (u32::MAX, 0, u32::MAX, 0),
         |(xmin, xmax, ymin, ymax), (x, y)| (xmin.min(*x), xmax.max(*x), ymin.min(*y), ymax.max(*y)),
     );
 
-    let mut matrix = Matrix2D {
-        width: 1_000,
-        height: ymax as usize + 3,
-        values: vec![' '; 1_000 * (ymax as usize + 3)],
-    };
-
-    fill_matrix(&mut matrix, &segments);
-
-    let mut count = 0;
-    for _ in 0..100_000 {
-        count += 1;
-        if !fall_point(&mut matrix, true) {
-            break;
-        }
-    }
-
-    println!("Part 2: {}", count - 1);
+    println!(
+        "Part 1: {}",
+        run_part(&segments, ymax as usize, Part::Part1)
+    );
+    println!(
+        "Part 2: {}",
+        run_part(&segments, ymax as usize, Part::Part2)
+    );
 }
