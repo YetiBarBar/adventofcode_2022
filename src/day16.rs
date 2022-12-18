e2ac98939c2e47078d673ac17d5da6b561c112c2
YetiBarBar <yetibarbar@gmail.com>
2022-12-18 13:04:45 +0100

    Add parsing for day 16

--- src/day16.rs
+++ src/day16.rs
@@ -1,3 +1,48 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha0, alpha1, char, newline, space1, u64},
+    combinator::map,
+    multi::{separated_list0, separated_list1},
+    sequence::{delimited, preceded, tuple},
+    IResult,
+};
+
+#[derive(Debug)]
+struct Valve<'a> {
+    name: &'a str,
+    rate: u64,
+    connected_valve: Vec<&'a str>,
+}
+
+fn valve(data: &str) -> IResult<&str, Valve> {
+    map(
+        tuple((
+            delimited(tag("Valve "), alpha0, space1),
+            delimited(tag("has flow rate="), u64, char(';')),
+            alt((
+                preceded(
+                    tag(" tunnels lead to valves "),
+                    separated_list1(tag(", "), alpha1),
+                ),
+                preceded(
+                    tag(" tunnel leads to valve "),
+                    separated_list0(tag(", "), alpha1),
+                ),
+            )),
+        )),
+        |(name, rate, connected_valve)| Valve {
+            name,
+            rate,
+            connected_valve,
+        },
+    )(data)
+}
+
+const DURATION: usize = 30;
+
 pub fn main() {
-    todo!()
+    let valve = separated_list1(newline, valve)(include_str!("../data/day_2022_16.data")).unwrap();
+
+    println!("{:#?}", valve.1);
 }
